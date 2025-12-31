5918e5954d7c95a616d125db428e4fd81ed65f21
Yirui Chen <chenyirui@whu.edu.cn>
2025-12-31 14:55:54 +0800

    contact info found

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
     std::cout << "Hello Yirui!" << std::endl;
 
 
-
+    std::cout << "contact info found." << std::endl;
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
